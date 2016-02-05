effeb4b7c7908521fa4e653e2f142904639c7153
Chengxi (Chen) Shi <chengxis@usc.edu>
2016-02-04 20:43:19 -0800

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -14,23 +14,33 @@ public:
 	}
 	
 	int getDistance(string word1, string word2){
-		vector<int> v1, v2;
-		if(myMap.find(word1) == myMap.end()){
-			return -1;
-		}else{
-			v1 = myMap[word1];
-		}
-		if(myMap.find(word2) == myMap.end()){
-			return -1;
-		}else{
-			v2 = myMap[word2];
-		}
 		int bestAns = INT_MAX;
-		for(int i = 0; i < v1.size() ; i ++){
-			for(int j = 0; j < v2.size() ; j ++){
-				int tmp = abs(v1[i] - v2[j]);
-				if(tmp < bestAns){
-					bestAns = tmp;
+		if(word1 == word2){
+			if(myMap.find(word1) == myMap.end()) return -1;
+			if(myMap[word1].size() == 1) return 0;
+			for(int i = 0; i < myMap[word1].size() - 1; i ++){
+				int tmp = abs(myMap[word1][i] - myMap[word1][i+1]);
+				if(tmp < bestAns) bestAns = tmp;
+			}
+		}else{
+			vector<int> v1, v2;
+			if(myMap.find(word1) == myMap.end()){
+				return -1;
+			}else{
+				v1 = myMap[word1];
+			}
+			if(myMap.find(word2) == myMap.end()){
+				return -1;
+			}else{
+				v2 = myMap[word2];
+			}
+			
+			for(int i = 0; i < v1.size() ; i ++){
+				for(int j = 0; j < v2.size() ; j ++){
+					int tmp = abs(v1[i] - v2[j]);
+					if(tmp < bestAns){
+						bestAns = tmp;
+					}
 				}
 			}
 		}
